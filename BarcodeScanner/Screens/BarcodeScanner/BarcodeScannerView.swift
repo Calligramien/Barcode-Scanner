62b3d21defffe1ce3a7f5a190e2875d1995d4f03
Damien Gautier <82036710+Calligramien@users.noreply.github.com>
2023-03-07 11:19:11 +0100

    [CHG] Cleaning + UI

--- BarcodeScanner/Screens/BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/Screens/BarcodeScanner/BarcodeScannerView.swift
@@ -14,8 +14,9 @@ struct BarcodeScannerView: View {
     var body: some View {
         NavigationView {
             VStack {
-                ScannerView(scannedCode: $viewModel.scannedCode, alertItem: $viewModel.alertItem)
-                    .frame(maxWidth: .infinity, maxHeight: 300)
+                ScannerView(scannedCode: $viewModel.scannedCode,
+                            alertItem: $viewModel.alertItem)
+                    .frame(maxHeight: 300)
                 
                 Spacer().frame(height: 60)
                 
@@ -36,8 +37,8 @@ struct BarcodeScannerView: View {
             }
             .navigationTitle("Barcode Scanner")
             .alert(item: $viewModel.alertItem) { alertItem in
-                Alert(title: Text(alertItem.title),
-                      message: Text(alertItem.message),
+                Alert(title: alertItem.title,
+                      message: alertItem.message,
                       dismissButton: alertItem.dismissButton)
             }
         }
